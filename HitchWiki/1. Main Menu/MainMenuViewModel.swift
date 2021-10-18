1e279a40ffea7b5a7ac6bd205fb7efa48fe4ec35
Kacper Cichosz <kcichosz@nn4m.com>
2021-10-18 12:50:29 +0100

    Infobox set up
    - Set up infobox and populate it with data
    - Implement MVVM
    - Make existing pages clickable inside infobox

--- HitchWiki/1. Main Menu/MainMenuViewModel.swift
+++ HitchWiki/1. Main Menu/MainMenuViewModel.swift
@@ -5,12 +5,54 @@
 //  Created by Kacper Cichosz on 03/10/2021.
 //
 
-import Foundation
+import UIKit
 
-class MainMenuViewModel {
-    var pages = [Page]()
+class MainMenuViewModel: ViewModel, ViewModelProtocol {
     
-    func loadData(completion: () -> Void) {
-        // TODO: Make the viewModel to load the data
+    var pages = [Page]()
+    var filteredPages = [Page]()
+    let stringsToExclude = ["Main Page", "MediaWiki","User","Template","Hitch","Category","Top","Help","phrasebook","Talk","What to","How to","File:","Encyclopedia","Academy","Sign Up","Portal:","Resources"]
+    
+    
+    // MARK: - Updates
+    var update: ((MainMenuViewModel.UpdateType) -> Void)?
+    enum UpdateType {
+        case search
+        case mainMenu
+    }
+    
+    var error: ((MainMenuViewModel.ErrorType) -> Void)?
+    enum ErrorType {
+        
+    }
+    
+    public func searchBarClicked() {
+        self.update?(.search)
+    }
+    
+    public func loadData() {
+        NetworkManager().readLocalData(forName: "hitchwiki") { [weak self] result in
+            switch result {
+            case .success(var pagesToDisplay):
+                self?.stringsToExclude.forEach({ (item) in
+                    pagesToDisplay = pagesToDisplay.filter({ (page) -> Bool in
+                        return !page.title.lowercased().contains(item.lowercased())
+                    })
+                })
+                self?.pages = pagesToDisplay
+                self?.filteredPages = pagesToDisplay
+            case .failure(let error):
+                // TODO: Alert once the Alert Manager is set up
+                print("\(error)")
+            }
+        }
+    }
+    
+    func moveToPageScreen(indexRow: Int) {
+        let page = self.filteredPages[indexRow]
+        let pageDetailViewController = PageDetailViewController(viewModel: PageDetailViewModel())
+        pageDetailViewController.populatePage(page: page, allPages: self.pages)
+        pageDetailViewController.modalPresentationStyle = .fullScreen
+        UIApplication.topViewController()?.navigationController?.pushViewController(pageDetailViewController, animated: true)
     }
 }
